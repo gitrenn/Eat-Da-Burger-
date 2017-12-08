40be94681ce77b74ddc971bdc262767553b5a78c
gitrenn <rjingmembers@gmail.com>
2017-12-07 21:05:58 -0800

    added files, folders and dependencies

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('grilled chicken', true, '2017-12-02 02:13:12');
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('bbq chicken', false, '2017-12-02 02:13:12');
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('homestyle', false, '2017-12-02 02:13:12');
+INSERT INTO burgers (burger_name, devoured, date) 
+VALUES ('canjun style', false, '2017-12-02 02:13:12');
+
---
+++ server.js
@@ -0,0 +1,3 @@
+var express = require("express");
+var methOver = require("method-override");
+var bodyParser = require("body-parser");
